8e13eefc77a7273a468cacfc55280530b4b9eb24
poxiest <vishaak98@gmail.com>
2025-04-18 18:01:15 -0400

    Bug fixed

--- Calendar_App/src/main/java/controller/ControllerWithView.java
+++ Calendar_App/src/main/java/controller/ControllerWithView.java
@@ -397,8 +397,8 @@ public class ControllerWithView extends Controller implements IControllerWithVie
           visitor.getAverageEventsPerDay(),
           visitor.getMostBusyByEvents(),
           visitor.getLeastBusyByEvents(),
-          visitor.getMostBusyByHours(),
-          visitor.getLeastBusyByHours(),
+          visitor.getMostBusyByDuration(),
+          visitor.getLeastBusyByDuration(),
           visitor.getOnlinePercentage(),
           visitor.getOfflinePercentage()
       );
